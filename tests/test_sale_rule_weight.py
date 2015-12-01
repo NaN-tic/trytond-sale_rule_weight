e684c05847061dc23064211d8006758eb364c5f7
resteve <resteve@zikzakmedia.com>
2015-12-01 11:22:15 +0100

    Tests

--- tests/test_sale_rule_weight.py
+++ tests/test_sale_rule_weight.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the sale_rule_weight module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleRuleWeightTestCase(unittest.TestCase):
-    'Test Sale Rule module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_rule_weight')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class SaleRuleWeightTestCase(ModuleTestCase):
+    'Test Sale Rule Weight module'
+    module = 'sale_rule_weight'
 
 
 def suite():
