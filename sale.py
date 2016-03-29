928417a10a01eec36a02ccaf4ac922b478532014
jmartin <jmartin@zikzakmedia.com>
2016-03-29 12:00:41 +0200

    Add python 3 support

--- sale.py
+++ sale.py
@@ -4,10 +4,10 @@
 from trytond.pool import PoolMeta
 
 __all__ = ['SaleRuleCondition']
-__metaclass__ = PoolMeta
 
 
 class SaleRuleCondition:
+    __metaclass__ = PoolMeta
     __name__ = 'sale.rule.condition'
 
     @classmethod
